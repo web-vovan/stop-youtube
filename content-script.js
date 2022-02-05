7b59fff9cfca80e4ba6e844a59edc738bdfcb684
vladimir <myweb11@mail.ru>
2022-02-05 17:06:54 +0300

    включение/отключение расширения

--- content-script.js
+++ content-script.js
@@ -1,1 +1,14 @@
-document.body.innerHTML = '<div class="wrap"><div class="title">Stop youtube</div></div>';
+chrome.storage.sync.get("active", function({ active }) {
+    if (active === true) {
+        document.body.innerHTML = '<div class="wrap"><div class="title">Stop youtube</div></div>';
+    }
+});
+
+
+
+
+// chrome.storage.sync.get("color", function({ color }) {
+//     console.log('Значение переменной: ' + color);
+// });
+
+
--- popup.js
+++ popup.js
@@ -1,46 +1,15 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let changeButton = document.getElementById("changeButton");
 
-chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
+chrome.storage.sync.get("active", function({ active }) {
+    if (active === true) {
+        changeButton.textContent = "Выключить"
+    } else {
+        changeButton.textContent = "Включить"
+    }
+
+    changeButton.addEventListener('click', function () {
+        chrome.storage.sync.set({
+            active: !active
+        })
+    })
 });
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: setPageBackgroundColor,
-    });
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
-}
-
-// chrome.tabs.query(
-//     {
-//         active: true,
-//         currentWindow: true
-//     },
-//     ([currentTab]) => {
-//         const url = new URL(currentTab.url);
-//         const domain = url.hostname;
-//         console.log(domain);
-//     }
-// );
-
-// let test = document.getElementById('domain');
-// test.textContent('hello');
-
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var tab = tabs[0];
-    var url = new URL(tab.url)
-    var domain = url.hostname
-    // `domain` now has a value like 'example.com'
-})
