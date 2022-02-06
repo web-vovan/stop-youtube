4cb09fccfc68de641676ae0c6dff2f56320e6091
vladimir <myweb11@mail.ru>
2022-02-06 22:19:29 +0300

    сменил иконки

--- popup.js
+++ popup.js
@@ -3,14 +3,16 @@ let startTimeField = document.getElementById("startTimeField");
 let endTimeField = document.getElementById("endTimeField");
 
 chrome.storage.sync.get(['active', 'startTime', 'endTime'], function({active, startTime, endTime}) {
-    changeButton.classList.add('active')
+    if (active) {
+        changeButton.classList.add('block')
+    }
 
     changeButton.addEventListener('click', function () {
         chrome.storage.sync.set({
             active: !active
         })
 
-        changeButton.classList.toggle('active')
+        changeButton.classList.toggle('block')
     })
 
     startTimeField.value = startTime
